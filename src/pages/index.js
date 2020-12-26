08abf78b7c55d3fc35263572cbd5de8f053f74cd
yoshua70 <allidjinoumichee@outlook.fr>
2020-12-26 20:23:27 +0000

    Add Netlify Identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,18 @@ import Layout from "../components/layout"
 import PostLink from "../components/post-link"
 import HeroHeader from "../components/heroHeader"
 
+// Imports Netlify Identity script into HTML of site
+// Must be called from React life-cycle function
+export function initNetlifyIdentity() {
+  console.log("initNetlifyIdentity called.");
+  const script = document.createElement("script");
+
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+  script.async = true;
+
+  document.body.appendChild(script);
+}
+
 const IndexPage = ({
   data: {
     site,
@@ -12,6 +24,7 @@ const IndexPage = ({
   },
 }) => {
 
+  initNetlifyIdentity();
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
